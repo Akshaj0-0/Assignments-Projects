52c90f344efb7e362e12431e53fd8a955ca5821d
Akshaj Dwivedi <akshaj.shubh@gmail.com>
2025-08-27 10:04:22 +0530

    Create Write a program to demonstrate implicit type conversion in an expression.c

---
+++ Write a program to demonstrate implicit type conversion in an expression.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i = 10;
+    float f = 3.5;
+    double d;
+    d = i + f;
+    printf("Integer value (i): %d\n", i);
+    printf("Float value (f): %.2f\n", f);
+    printf("Result stored in double (d = i + f): %.2f\n", d);
+
+
+    return 0;

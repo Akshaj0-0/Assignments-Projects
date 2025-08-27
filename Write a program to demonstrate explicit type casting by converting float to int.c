412544913cbc173e7c735a6f6d1dd6c97f1b5f5c
Akshaj Dwivedi <akshaj.shubh@gmail.com>
2025-08-27 10:05:26 +0530

    Create Write a program to demonstrate explicit type casting by converting float to int.c

---
+++ Write a program to demonstrate explicit type casting by converting float to int.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    float f = 5.60;
+    int i;
+    i = (int)f;
+     printf("Original float value: %.2f\n", f);
+    printf("After casting to int: %d\n", i);
+  return 0;
+}
+
+
+
